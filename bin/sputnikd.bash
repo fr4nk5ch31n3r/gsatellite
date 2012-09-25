99206f73b72f18c662b20db10011a9b5f88d5023
Frank Scheiner <frank.scheiner@web.de>
2012-09-25 21:02:43 +0200

    Changed sputnikd to make a sputnik a session leader (setsid).

--- bin/sputnikd.bash
+++ bin/sputnikd.bash
@@ -3,7 +3,10 @@
 #  sputnikd - daemon like tool to start sputnik in the background without
 #+ binding it to a tty.
 
-nohup sputnik "$@" &>/dev/null &
+#nohup sputnik "$@" &>/dev/null &
+
+# start a new session for every sputnik, so the sputnik is the session leader.
+nohup setsid sputnik "$@" &>$HOME/.gsatellite/var/log/$( hostname --fqdn )/$$_sputnik.log &
 
 exit
 
