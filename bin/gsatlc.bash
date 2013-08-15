4c38b91e995e978109e4076fa9d3e377e88ebdc4
Frank Scheiner <scheiner@hlrs.de>
2013-08-15 17:26:33 +0200

    Enhanced tools to be able to run directly from the git repo. Added functionality for delegated actions. Gsatlc now delegates an action to sigfwd to update the timestamps of it`s PID file as long as it is running. As gsatlc is paused most of the time, it cannot do this by itself but client tools should be enabled to detect if it is still running, even if clients are on remote hosts. Internal cleanup for gsatlc and sigfwd.

--- bin/gsatlc.bash
+++ bin/gsatlc.bash
@@ -7,6 +7,7 @@
 :<<COPYRIGHT
 
 Copyright (C) 2011, 2012 Frank Scheiner
+Copyright (C) 2013 Frank Scheiner, HLRS, Universitaet Stuttgart
 
 The program is distributed under the terms of the GNU General Public License
 
@@ -29,30 +30,59 @@ umask 0077
 
 _DEBUG="0"
 
+_program=$( basename "$0" )
+
+readonly __GLOBAL__version="0.2.0"
+
 _gsatlcInboxName="gsatlc.inbox"
 _gsatBaseDir=$HOME/.gsatellite
 
 _scheduler="fifo"
 
-#  path to path configuration file (prefer system paths!)
-if [[ -e "/opt/gsatellite/etc/paths.conf" ]]; then
-        _pathsConfigurationFile="/opt/gsatellite/etc/paths.conf"
-#sed#elif [[ -e "<PATH_TO_GSATELLITE>/etc/paths.conf" ]]; then
-#sed#    _pathsConfigurationFile="<PATH_TO_GSATELLITE>/etc/paths.conf"
-elif [[ -e "/etc/opt/gsatellite/etc/paths.conf" ]]; then
-        _pathsConfigurationFile="/etc/opt/gsatellite/etc/paths.conf"
-elif [[ -e "$HOME/.gsatellite/paths.conf" ]]; then
-        _pathsConfigurationFile="$HOME/.gsatellite/paths.conf"
+################################################################################
+
+#  path to configuration files (prefer system paths!)
+#  For native OS packages:
+if [[ -e "/etc/gsatellite" ]]; then
+        _configurationFilesPath="/etc/gsatellite"
+
+#  For installation with "install.sh".
+#sed#elif [[ -e "<PATH_TO_GSATELLITE>/etc" ]]; then
+#sed#	_configurationFilesPath="<PATH_TO_GSATELLITE>/etc"
+
+#  According to FHS 2.3, configuration files for packages located in "/opt" have
+#+ to be placed here (if you use a provider super dir below "/opt" for the
+#+ gtransfer files, please also use the same provider super dir below
+#+ "/etc/opt").
+#elif [[ -e "/etc/opt/<PROVIDER>/gsatellite" ]]; then
+#	_configurationFilesPath="/etc/opt/<PROVIDER>/gsatellite"
+elif [[ -e "/etc/opt/gsatellite" ]]; then
+        _configurationFilesPath="/etc/opt/gsatellite"
+
+# For git deploy, use $BASH_SOURCE
+elif [[ -e "$( dirname $BASH_SOURCE )/../etc" ]]; then
+	_configurationFilesPath="$( dirname $BASH_SOURCE )/../etc"
+
+#  For user install in $HOME:
+elif [[ -e "$HOME/.gsatellite" ]]; then
+        _configurationFilesPath="$HOME/.gsatellite"
 fi
 
-#  include path config
-. "$_pathsConfigurationFile"
+_pathsConfigurationFile="$_configurationFilesPath/paths.conf"
+
+#  include path config or fail with EX_SOFTWARE = 70, internal software error
+#+ not related to OS
+if ! . "$_pathsConfigurationFile"; then
+	echo "($_program) E: Paths configuration file couldn't be read or is corrupted." 1>&2
+	exit 70
+fi
 
 #. "$_LIB"/ipc.bashlib
 #. "$_LIB"/ipc/file.bashlib
 . "$_LIB"/ipc/file/sigfwd.bashlib
 . "$_LIB"/ipc/file/msgproc.bashlib
 . "$_LIB"/gsatlc.bashlib
+. "$_LIB"/utils.bashlib
 
 #  reimplementation with if clauses, as this could allow to include
 #+ functionality dynamically
@@ -82,15 +112,61 @@ processMsg() {
         return
 }
 
+#  onExit() - perform cleanup on exit.
+gsatlc/onExit()
+{
+	#gsatlc/stopAllJobs
+
+	#  remove all message boxes
+	ipc/file/removeMsgBox "$_inbox"
+
+	if [[ ! -z $_actionId ]]; then
+		ipc/file/sigfwd/undelegateAction "$_actionId"
+	fi
+
+	# remove state files if still existing
+	rm -f "$_gsatBaseDir/gsatlcHostName" "$_gsatBaseDir/gsatlcPid"
+
+	sigfwdd --stop && echo "($$) [gsatlc] Signal forwarding stopped."
+	echo "($$) [gsatlc] Shutting down."
+
+	return
+}
+
+
+gsatlc/prepareAction()
+{
+	local _actionTemplate="$1"
+	local _pidFile="$2"
+	local _period="$3"
+
+	mkdir -p "$_gsatBaseDir/tmp"
+
+	local _action="$_gsatBaseDir/tmp/gsatlc_action_$$"
+
+	sed -e "s|<PID_FILE>|$_pidFile|" -e "s|<PERIOD_IN_SECONDS>|$_period|" "$_actionTemplate" > "$_action"
+
+	if [[ $? == 0 ]]; then
+		echo "$_action"
+		return 0
+	else
+		return 1
+	fi
+}
 ################################################################################
+
+_actionTemplate="${_GSAT_LIBEXECPATH}/actions/templates/periodicPidFileUpdate.bash"
+# run action every 4 seconds
+_actionPeriod=4
 
 #  TODO:
 #+ On exit, stop (all) running job(s). On start, start jobs depending on the
 #+ scheduler.
 
 #  setup trap to remove inbox on exit
-trap 'ipc/file/removeLocalMsgBoxByName "$_inboxName"; sigfwdd --stop && echo "($$) Signal forwarding stopped."; echo "($$) Shutting down."' EXIT
 #trap 'ipc/file/removeLocalMsgBoxByName "$_inboxName"; ipc/file/sigfwd/stopSigfwd' EXIT
+#trap 'ipc/file/removeLocalMsgBoxByName "$_inboxName"; sigfwdd --stop && echo "($$) Signal forwarding stopped."; echo "($$) Shutting down."' EXIT
+trap 'gsatlc/onExit' EXIT
 
 #  Startup
 _self="$$"
@@ -101,15 +177,46 @@ _inboxName="$_self.inbox"
 _inbox=$( ipc/file/createMsgBox "$_inboxName" )
 
 #  save hostname, pid, etc.
-echo $( hostname --fqdn ) > "$_gsatBaseDir/gsatlcHostName"
+_hostName=$( utils/getHostName )
+mkdir -p "$_gsatBaseDir/var/run/$_hostName"
+echo "$_hostName" > "$_gsatBaseDir/gsatlcHostName"
 echo $_self > "$_gsatBaseDir/gsatlcPid"
 
 #  start signal forwarder
-#ipc/file/sigfwd/startSigfwd &>/dev/null
-sigfwdd --start
+sigfwdd --start && echo "($$) [gsatlc] Signal forwarding started."
 
-echo "($$) Signal forwarding started."
-echo "($$) Started up."
+# Delegate periodic update of PID file with sigfwd.
+#
+# This could work as follows, a process can provide a script file that is made
+# available to sigfwd. Sigfwd will then execute the script file in the
+# background and give back an ID identifying the specific action. By using this
+# ID, an action can later be unregistered.
+
+_action=$( gsatlc/prepareAction "$_actionTemplate" "$_gsatBaseDir/gsatlcPid" "$_actionPeriod" )
+
+if [[ $? != 0 ]]; then
+
+	echo "($$) [gsatlc] Periodic PID file update could not be pepared. Exiting."
+	exit 1
+else
+	echo "($$) [gsatlc] Periodic PID file update pepared."
+
+	sleep 1
+
+	_actionId=$( ipc/file/sigfwd/delegateAction "$_hostName" "$_action" )
+
+	if [[ $? != 0 ]]; then
+
+		echo "($$) [gsatlc] Periodic PID file update could not be delegated. Exiting."
+		exit 1
+	else
+		echo "($$) [gsatlc] Periodic PID file update delegated."
+	fi
+fi
+
+echo "($$) [gsatlc] Started up."
+
+#gsatlc/startAllJobs
 
 while [[ 1 ]]; do
         if ipc/file/messageAvailable "$_inbox"; then
